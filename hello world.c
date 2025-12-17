8f8e099a330fdc9a5cebf3daca1a499e9ecc90d6
punamchand wagh <punamchandwagh@gmail.com>
2025-12-17 22:29:15 +0530

    Create hello world.c

---
+++ hello world.c
@@ -0,0 +1,8 @@
+* What is C:--->C is a general-purpose, procedural programming language developed by Dennis Ritchie in 1972 at Bell Laboratories.
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
